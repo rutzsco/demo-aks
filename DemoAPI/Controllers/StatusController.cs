38edeafc901339077a02497a660871be923c8f40
Scott Rutz <scrutz@microsoft.com>
2021-04-22 11:24:13 -0500

    Create StatusController.cs

---
+++ DemoAPI/Controllers/StatusController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StatusController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "OK";
+        }
+    }
+}
